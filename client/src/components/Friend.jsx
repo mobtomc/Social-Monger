a3b9be862eceb42e0537fc966d3f923304561ea2
aryan bhat <100569334+mobtomc@users.noreply.github.com>
2023-09-06 18:25:40 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://backs-a6ko.onrender.com//users/${_id}/${friendId}`,
+      `https://backs-a6ko.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
